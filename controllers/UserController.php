4507a55b5db5a1d1ee1c72924e86794bd03725d5
Daniel R. Vásquez Montes <dev.DanielR@gmail.com>
2021-06-12 01:46:11 -0500

    Ordered CRUD actions & moved search into index

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -15,12 +15,6 @@ use yii\filters\VerbFilter;
  */
 class UserController extends Controller
 {
-    static $flashMessges = [
-        'create' => 'User created succesfully.',
-        'update' => 'User updated sucessfully.',
-        'delete' => 'User deleted sucessfully.'
-    ];
-
     /**
      * {@inheritdoc}
      */
@@ -49,8 +43,19 @@ class UserController extends Controller
      */
     public function actionIndex()
     {
-        $dataProvider = $this->search(Yii::$app->request->queryParams);
-        return $this->render('index', ['dataProvider' => $dataProvider]);
+        $params = Yii::$app->request->queryParams;
+        $query  = User::find()
+            ->andFilterWhere([
+                'id'           => $params['id']           ?? null,
+                'is_active'    => $params['is_active']    ?? null,
+                'is_validated' => $params['is_validated'] ?? null])
+            ->andFilterWhere(['like', 'name', $params['name'] ?? null])
+            ->andFilterWhere(['like', 'main_email', $params['main_email'] ?? null])
+            ->andFilterWhere(['like', 'backup_email', $params['name'] ?? null]);
+
+        return $this->render('index', [
+            'dataProvider' => new ActiveDataProvider(['query' => $query])
+        ]);
     }
 
     /**
@@ -59,34 +64,23 @@ class UserController extends Controller
      * @return mixed
      * @throws NotFoundHttpException if the model cannot be found
      */
-    public function actionView($id)
-    {
-        return $this->render('view', ['model' => $this->findModel($id)]);
-    }
+    public function actionView($id) { return $this->helperCRUD('view', $id); }
 
     /**
      * Creates a new User model.
-     * If creation is successful, the browser will be redirected to the 'view' page.
+     * If creation is successful, the browser will be redirected to the 'index' page.
      * @return mixed
      */
-    public function actionCreate()
-    {
-        $model = new User();
-        return $this->helperForm('create', $model);
-    }
+    public function actionCreate() { return $this->helperCRUD('create'); }
 
     /**
      * Updates an existing User model.
-     * If update is successful, the browser will be redirected to the 'view' page.
+     * If update is successful, the browser will be redirected to the 'index' page.
      * @param integer $id
      * @return mixed
      * @throws NotFoundHttpException if the model cannot be found
      */
-    public function actionUpdate($id)
-    {
-        $model = $this->findModel($id);
-        return $this->helperForm('update', $model);
-    }
+    public function actionUpdate($id) { return $this->helperCRUD('update', $id); }
 
     /**
      * Deletes an existing User model.
@@ -95,60 +89,53 @@ class UserController extends Controller
      * @return mixed
      * @throws NotFoundHttpException if the model cannot be found
      */
-    public function actionDelete($id)
+    public function actionDelete($id) { return $this->helperCRUD('delete', $id); }
+
+    /**
+     * Set session flash.
+     */
+    protected function setFlash($action)
     {
-        $this->findModel($id)->delete();
-        Yii::$app->session->setFlash('success', "User deleted successfully.");
-        return $this->redirect(['index']);
+        $flashMessges = [
+            'create' => 'User created succesfully.',
+            'update' => 'User updated sucessfully.',
+            'delete' => 'User deleted sucessfully.'
+        ];
+        Yii::$app->session->setFlash('success', Yii::t('app', $flashMessges[$action]));
     }
 
     /**
-     * Finds the User model based on its primary key value.
-     * If the model is not found, a 404 HTTP exception will be thrown.
-     * @param integer $id
-     * @return User the loaded model
-     * @throws NotFoundHttpException if the model cannot be found
+     * Helps render for CRUD actions.
      */
-    protected function findModel($id)
+    protected function helperCRUD($action, $id = NULL)
     {
-        if (($model = User::findOne($id)) !== null) return $model;
-        throw new NotFoundHttpException(Yii::t('app', 'The requested page does not exist.'));
-    }
-
-    /**
-     * Creates data provider instance with search query applied
-     * @param array $params
-     * @return ActiveDataProvider
-     */
-    protected function search($params)
-    {
-        $query        = User::find();
-        $dataProvider = new ActiveDataProvider(['query' => $query]);
-        $query->andFilterWhere([
-            'id'           => $params['id']           ?? null,
-            'is_active'    => $params['is_active']    ?? null,
-            'is_validated' => $params['is_validated'] ?? null,
-        ]);
-        $query->andFilterWhere(['like', 'name', $params['name'] ?? null]);
-        $query->andFilterWhere(['like', 'main_email', $params['main_email'] ?? null]);
-        $query->andFilterWhere(['like', 'backup_email', $params['name'] ?? null]);
-        return $dataProvider;
-    }
-
-    /**
-     * Helps render form for Create & Update actions.
-     */
-    protected function helperForm($action, $model)
-    {
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            Yii::$app->session->setFlash('success',
-                Yii::t('app', static::$flashMessages[$action]));
-            return $this->redirect(['index']);
+        //Find model
+        switch ($action) {
+            case 'create': $model = new User(); break;
+            default:       $model = User::findOne($id);
         }
+        //Throw error if model not found
+        if ($model === null) throw new NotFoundHttpException(Yii::t('app', 'The requested page does not exist.'));
 
-        return $this->render('_form', [
-            'action' => $action,
-            'model'  => $model
-        ]);
+        //Handle actions
+        switch ($action) {
+
+            //Render view
+            case 'view': return $this->render('view', ['model' => $model]);
+
+            //Handle delete
+            case 'delete':
+                $model->delete();
+                $this->setFlash('delete');
+                return $this->redirect(['index']);
+
+            //Handle & render create & update
+            default:
+                if ($model->load(Yii::$app->request->post()) && $model->save()) {
+                    $this->setFlash($action);
+                    return $this->redirect(['index']);
+                }
+                return $this->render('_form', ['action' => $action, 'model' => $model]);
+        }
     }
 }
