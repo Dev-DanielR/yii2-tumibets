86d6cc845ba42119782f5a293eb6609ae8c9a7d0
Daniel R. Vásquez Montes <dev.DanielR@gmail.com>
2021-06-17 16:28:58 -0500

    Ordered & added translations

--- messages/es-PE/app.php
+++ messages/es-PE/app.php
@@ -1,63 +1,100 @@
 <?php
-    $objects = [
-        'Home'             => 'Inicio',
-        'About'            => 'Nosotros',
-        'Contact'          => 'Contactanos',
-        'Team'             => 'Equipo',
-        'Teams'            => 'Equipos',
-        'Tournament'       => 'Torneo',
-        'Tournaments'      => 'Torneos',
-        'Tournament Date'  => 'Jornada',
-        'Tournament Dates' => 'Jornadas',
-        'Fixture'          => 'Fixture',
-        'Fixtures'         => 'Fixtures',
-        'Bet'              => 'Apuesta',
-        'Bets'             => 'Apuestas',
-        'User'             => 'Usuario',
-        'Users'            => 'Usuarios',
-        'User Session'     => 'Sesión de Usuario',
-        'User Sessions'    => 'Sesiones de Usuario',
+    $bet = [
+        'Bet'        => 'Apuesta',
+        'Bets'       => 'Apuestas',
+        'View Bet'   => 'Ver Apuesta',
+        'Create Bet' => 'Crear Apuesta',
+        'Update Bet' => 'Actualizar Apuesta',
+        'Delete Bet' => 'Eliminar Apuesta',
+        'Bet created succesfully.' => 'Apuesta creada exitosamente.',
+        'Bet updated sucessfully.' => 'Apuesta actualizada exitosamente.',
+        'Bet deleted sucessfully.' => 'Apuesta eliminada exitosamente.',
     ];
 
-    $actions = [
-        'Login'                  => 'Ingresar',
-        'Logout'                 => 'Salir',
-        'View'                   => 'Ver',
-        'Create'                 => 'Crear',
-        'Update'                 => 'Actualizar',
-        'Delete'                 => 'Eliminar',
-        'Save'                   => 'Guardar',
-        'View Team'              => 'Ver Equipo',
-        'Create Team'            => 'Crear Equipo',
-        'Update Team'            => 'Actualizar Equipo',
-        'Delete Team'            => 'Eliminar Equipo',
-        'View Tournament'        => 'Ver Torneo',
-        'Create Tournament'      => 'Crear Torneo',
-        'Update Tournament'      => 'Actualizar Torneo',
-        'Delete Tournament'      => 'Delete Torneo',
+    $fixture = [
+        'Fixture'        => 'Fixture',
+        'Fixtures'       => 'Fixtures',
+        'View Fixture'   => 'Ver Fixture',
+        'Create Fixture' => 'Crear Fixture',
+        'Update Fixture' => 'Actualizar Fixture',
+        'Delete Fixture' => 'Eliminar Fixture',
+        'Fixture created succesfully.' => 'Fixture creado exitosamente.',
+        'Fixture updated sucessfully.' => 'Fixture actualizado exitosamente.',
+        'Fixture deleted sucessfully.' => 'Fixture eliminado exitosamente.',
+    ];
+
+    $team = [
+        'Team'        => 'Equipo',
+        'Teams'       => 'Equipos',
+        'View Team'   => 'Ver Equipo',
+        'Create Team' => 'Crear Equipo',
+        'Update Team' => 'Actualizar Equipo',
+        'Delete Team' => 'Eliminar Equipo',
+        'Team created succesfully.' => 'Equipo creado exitosamente.',
+        'Team updated sucessfully.' => 'Equipo actualizado exitosamente.',
+        'Team deleted sucessfully.' => 'Equipo eliminado exitosamente.',
+    ];
+
+    $tournament = [
+        'Tournament'        => 'Torneo',
+        'Tournaments'       => 'Torneos',
+        'View Tournament'   => 'Ver Torneo',
+        'Create Tournament' => 'Crear Torneo',
+        'Update Tournament' => 'Actualizar Torneo',
+        'Delete Tournament' => 'Eliminar Torneo',
+        'Tournament created succesfully.' => 'Torneo creado exitosamente.',
+        'Tournament updated sucessfully.' => 'Torneo actualizado exitosamente.',
+        'Tournament deleted sucessfully.' => 'Torneo eliminado exitosamente.',
+    ];
+
+    $tournament_date = [
+        'Tournament Date'        => 'Jornada',
+        'Tournament Dates'       => 'Jornadas',
         'View Tournament Date'   => 'Ver Jornada',
         'Create Tournament Date' => 'Crear Jornada',
         'Update Tournament Date' => 'Actualizar Jornada',
-        'Delete Tournament Date' => 'Delete Jornada',
-        'View Fixture'           => 'Ver Fixture',
-        'Create Fixture'         => 'Crear Fixture',
-        'Update Fixture'         => 'Actualizar Fixture',
-        'Delete Fixture'         => 'Delete Fixture',
-        'View Bet'               => 'Ver Apuesta',
-        'Create Bet'             => 'Crear Apuesta',
-        'Update Bet'             => 'Actualizar Apuesta',
-        'Delete Bet'             => 'Delete Apuesta',
-        'View User'              => 'Ver Usuario',
-        'Create User'            => 'Crear Usuario',
-        'Update User'            => 'Actualizar Usuario',
-        'Delete User'            => 'Delete Usuario',
+        'Delete Tournament Date' => 'Eliminar Jornada',
+        'Tournament Date created succesfully.' => 'Jornada creada exitosamente.',
+        'Tournament Date updated sucessfully.' => 'Jornada actualizada exitosamente.',
+        'Tournament Date deleted sucessfully.' => 'Jornada eliminada exitosamente.',
+    ];
+
+    $user = [
+        'User'        => 'Usuario',
+        'Users'       => 'Usuarios',
+        'View User'   => 'Ver Usuario',
+        'Create User' => 'Crear Usuario',
+        'Update User' => 'Actualizar Usuario',
+        'Delete User' => 'Eliminar Usuario',
+        'User created succesfully.' => 'Usuario creado exitosamente.',
+        'User updated sucessfully.' => 'Usuario actualizado exitosamente.',
+        'User deleted sucessfully.' => 'Usuario eliminado exitosamente.',
+    ];
+
+    $user_session = [
+        'User Session'      => 'Sesión de Usuario',
+        'User Sessions'     => 'Sesiones de Usuario',
+        'View User Session' => 'Ver Sesión de Usuario',
+    ];
+
+    $buttons = [
+        'Home'    => 'Inicio',
+        'About'   => 'Nosotros',
+        'Contact' => 'Contactanos',
+        'Login'   => 'Ingresar',
+        'Logout'  => 'Salir',
+        'View'    => 'Ver',
+        'Create'  => 'Crear',
+        'Update'  => 'Actualizar',
+        'Delete'  => 'Eliminar',
+        'Save'    => 'Guardar',
     ];
 
     $labels = [
         'TumiBets'          => 'TumiBets',
         'Language'          => 'Lenguage',
         'ID'                => 'ID',
-        'Is Admin'          => 'Admin',
+        'Admin'             => 'Admin',
         'Is Active'         => 'Activo',
         'Is Validated'      => 'Validado',
         'Name'              => 'Nombre',
@@ -90,5 +127,6 @@
     ];
 
     return array_merge(
-        $objects, $actions, $labels, $texts
+        $bet, $fixture, $team, $tournament, $tournament_date, $user, $user_session,
+        $buttons, $labels, $texts
     );
