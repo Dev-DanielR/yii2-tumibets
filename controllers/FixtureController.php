1b2370dbd16eea12e1a521a56865f173a83c684a
Daniel R. Vásquez Montes <dev.DanielR@gmail.com>
2021-06-17 00:15:39 -0500

    Minor fix

--- controllers/FixtureController.php
+++ controllers/FixtureController.php
@@ -194,16 +194,16 @@ class FixtureController extends Controller
                 $dependencies = $this->getDependencies();
 
                 if ($model->load(Yii::$app->request->post()) && $model->save()) {
+                    $this->setFlash($action);
                     Yii::$app->mailer->compose()
                         ->setFrom('daniel.vasquez@tumi.com.pe')
                         ->setTo($model->userCreated->main_email)
                         ->setSubject(Yii::t('app', 'Tumibets: Fixture ' . $action . 'd'))
                         ->setHtmlBody('<h2>TUMIBETS</h2>
                             <p>'. Yii::t('app', 'A fixture was ' . $action . 'd for') . ': ' . $model->name .'</p>
-                            <p>'. Html::a(Yii::t('app', 'Check here.'),
+                            <p>'. Html::a(Yii::t('app', 'Check the fixture here.'),
                             [Url::to(['fixture/view']), 'id' => $model->id]) .'</p>')
                         ->send();
-                    $this->setFlash($action);
                     return $this->redirect(['index', 'tournament_date_id' => $model->tournament_date_id]);
                 }
 
